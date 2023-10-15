dda5db367485c68ea29d295791158e64e3a2af2e
Azat Aldeshov <azata1919@gmail.com>
2023-10-15 13:52:14 +0600

    Dockerising & pwa

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,7 @@ import App from './App';
 import './index.css';
 import theme from "./configs/Theme";
 import {ThemeProvider} from "@mui/material";
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 ReactDOM.render(
     <React.StrictMode>
@@ -19,6 +20,11 @@ ReactDOM.render(
     </React.StrictMode>,
     document.getElementById('root')
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
